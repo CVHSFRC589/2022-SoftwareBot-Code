065780586013e569a01d3902437426e0c4f9b309
Seon-Jae Yoon <54429021-SeonJaeY@users.noreply.github.com>
2022-01-20 19:07:55 -0800

    Encoders Added
    Added Encoders and methods to read/set the encoders to the Drive subsystem

--- TankDrive/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ TankDrive/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -10,16 +10,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
  
 public class DriveTrainSubsystem extends SubsystemBase {
-    //not entirely sure about these two??
     private final CANSparkMax m_leftMotor = new CANSparkMax(Constants.kLeftMotorPort, MotorType.kBrushless);
     private final CANSparkMax m_rightMotor = new CANSparkMax(Constants.kRightMotorPort, MotorType.kBrushless);
+
+    private final CANEncoder leftEncoder = m_leftMotor.getEncoder(EncoderType.kHallSensor, 42);
+    private final CANEncoder rightEncoder = m_rightMotor.getEncoder(EncoderType.kHallSensor, 42);
  
     private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    private final double encoderPerInch = 1;
     private double m_scaleFactor = 1.0;
  
     /**create a new drive train subsystem */
     public DriveTrainSubsystem() {
-        super();
+
+       leftEncoder.setPosition(0);
+       rightEncoder.setPosition(0);
+
+      
+      super();
     }
  
     public void drive(double left, double right) {
@@ -37,6 +45,26 @@ public class DriveTrainSubsystem extends SubsystemBase {
     public void goSlow()
     {
         m_scaleFactor = 0.5;
+    }
+
+    public double getLeft()
+    {
+        return leftEncoder.getPosition();
+    }
+
+    public double getRight()
+    {
+        return rightEncoder.getPosition();
+    }
+
+    public void setLeft(int position)
+    {
+        leftEncoder.setPosition(position);
+    }
+
+    public void setRight(int position)
+    {
+        rightEncoder.setPosition(position);
     }
       /** Call log method every loop. */
   @Override
